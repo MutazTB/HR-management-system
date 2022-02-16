3fc0c8dfce4c217a93b94b729e7d3f7055264d4e
MutazTB <mutazaltbakhi900@gmail.com>
2022-02-16 03:31:02 +0200

    events

--- js/app.js
+++ js/app.js
@@ -1,55 +1,109 @@
 "Use strict";
 
 var employees = [];
-function Employee(employee_ID , name , department ,level , image){
-    this.employee_ID = employee_ID ;
+function Employee( name , department ,level , image){
+    this.employee_ID = 1000  ;
     this.name = name;
     this.department = department;
     this.level = level ;
     this.image = image ;
+    this.salary = 0 ;
     employees.push(this);
 }
 
-let ghaziSamer = new Employee(1000 , "Ghazi Samer" , "Administration" , "Senior" , "url"  );
-let lanaAli = new Employee(1001 , "Lana Ali" , "Finance" , "Senior" , "url" );
-let tamaraAyoub = new Employee(1002 , "Tamara Ayoub" , "Marketing" , "Senior" , "url" );
-let safiWaleed = new Employee(1003 , "Safi Walid" , "Administration" , "Mid-Senior" , "url" );
-let omarZaid = new Employee(1003 , "Omar Zaid" , "Development" , "Senior" , "url" );
-let ranaSaleh = new Employee(1003 , "Rana Saleh" , "Administration" , "Junior" , "url" );
-let hadiAhmad = new Employee(1003 , "Hadi Ahmad" , "Finance" , "Mid-Senior" , "url" );
+let name = document.getElementById("#fullname");
+let department = document.getElementById("Department");
+let level = document.getElementsByName("Level");
+let salary = document.getElementById("#salary");
 
-function Salary() {
+let ghaziSamer = new Employee( "Ghazi Samer" , "Administration" , "Senior" , "url"  );
+let lanaAli = new Employee( "Lana Ali" , "Finance" , "Senior" , "url" );
+let tamaraAyoub = new Employee( "Tamara Ayoub" , "Marketing" , "Senior" , "url" );
+let safiWaleed = new Employee( "Safi Walid" , "Administration" , "Mid-Senior" , "url" );
+let omarZaid = new Employee( "Omar Zaid" , "Development" , "Senior" , "url" );
+let ranaSaleh = new Employee("Rana Saleh" , "Administration" , "Junior" , "url" );
+let hadiAhmad = new Employee("Hadi Ahmad" , "Finance" , "Mid-Senior" , "url" );
+
+Employee.prototype.getSalary = function() {
     var max ;
     var min ;
-    for( var i = 0 ; i<employees.length ; i++){  
         console.log(employees[i].level);      
-        if(employees[i].level == "Senior"){
+        if(this.level == "Senior"){
              min = 1500 ;
              max = 2000 ;
-             employees[i].salary = Math.random() * (max - min) + min;
         }
-        else if (employees[i].level == "Mid-Senior"){
+        else if (this.level == "Mid-Senior"){
              min = 1000 ;
              max = 1500 ;
-             employees[i].salary = Math.random() * (max - min) + min;
         }
         else {
              min = 500 ;
              max = 1000 ;
-             employees[i].salary = Math.random() * (max - min) + min;
         }
-        }        
-  }
-Salary();
+       let totalSalary = Math.random() * (max - min) + min; 
+        this.salary = totalSalary - totalSalary * 0.075;
+        } 
+
+
+        Employee.prototype.getId = function () {
+            for( var i = 0 ; i < employees.length ; i++){
+                employees[i].employee_ID += 1;
+            }        
+        }       
+
+
 Employee.prototype.render = function(){
-    document.write(`<h2 style = " margin-top : 30px ; margin-left : 200px ;color : red ; justify-content: center;">${this.name} : ${this.salary}</h2>`);
+    
+    //document.write(`<h2 style = " margin-top : 30px ; margin-left : 200px ;color : red ; justify-content: center;">${this.name} : ${this.salary}</h2>`);
+
+    let cardinfoooo = document.getElementById("mycardinfo");
+    let cardDiv =document.createElement("div");
+    cardDiv.className = "card";
+    cardinfoooo.appendChild(cardDiv);
+    let cardimgdiv =document.createElement("div");
+    cardimgdiv.className = "card-img";
+    cardDiv.appendChild(cardimgdiv);
+    let cardimg = document.createElement("img");
+    cardimg.src = "assets/Employee.jpg";
+    cardimg.className = "img-card";
+    cardimgdiv.appendChild(cardimg);
+    let cardinfo =document.createElement("div");
+    cardinfo.className = "card-info";
+    cardDiv.appendChild(cardinfo);
+    let cardpre =document.createElement("pre");
+    cardpre.className = "about-me";
+    cardinfo.appendChild(cardpre);
+    let b1 = document.createElement("p");
+    b1.textContent = "Name: " + this.name +" - ID: " +this.employee_ID;
+    cardpre.appendChild(b1);
+    let br = document.createElement("br");    
+    let b2 = document.createElement("p");
+    b2.textContent = "Department: " + this.department +"-Level: " + this.level  ;
+    cardpre.appendChild(b2);
+    //    
 }
 
-for( var i = 0 ; i < employees.length ; i++){
-employees[i].render();
-console.log(employees[i]);
+for( var i = 0 ; i < employees.length ; i++){     
+    employees[i].getId();   
+    employees[i].getSalary();
+    employees[i].render();
+    console.log(employees[i]);
 }
+let form = document.getElementById("dataForm");
+form.addEventListener("submit", handelSubmit);
 
+function handelSubmit(event){
+    event.preventDefault();
+    let name = event.target.name.value;
+    let department = event.target.Department.value;
+    let level = event.target.Level.value;
+    let image = event.target.image.value;
+    //
+    let newEmployee = new Employee(name, department , level , image);
+    newEmployee.getId();
+    newEmployee.render();    
+    form.reset();
+}
 
 
 
